725261436b5454469727ae6b57d288e0b63489be
elemyoav <elem@post.bgu.ac.il>
2023-11-22 19:28:29 +0200

    fixed package.json

--- app/src/app.js
+++ app/src/app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const app = express();
 
+// Routes
 app.get('/', (req, res) => {
   res.status(200).send('Hello, world!');
 });
 
+// Start server
 const PORT = 8080;
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
+// Export server for testing
 module.exports = server;
